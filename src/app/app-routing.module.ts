34d6dcbff89879d0b6c5172b12131012a43cb8ec
Andrea <astamariab@gmail.com>
2022-04-11 19:50:52 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { AboutModule } from './pages/about/about.module';
+import { AboutComponent } from './pages/about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  pathMatch: '',
+  loadChildren: () => import('src/app/pages/home/home.module').then(m => m.HomeModule)
+},
+{
+  path: 'list',
+  loadChildren: () => import('src/app/pages/productlist/productlist.module').then(m => m.ProductlistModule),
+  
+},
+{
+  path: 'list/:id',
+  loadChildren: () => import('src/app/pages/productdetail/productdetail.module').then(m => m.ProductdetailModule)
+},
+{
+  path: 'about',
+  loadChildren: () => import('src/app/pages/about/about.module').then(m => m.AboutModule)
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
